383e96d4f82ed3921c4e9daf8603d38958ac034b
Tiffany Huang <tiffany.jenyun.huang@gmail.com>
2020-01-11 17:50:21 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,5 @@
 # Use this space to code along with the readme
-Def say_hello_ten_times
+Def say_hello_world_ten_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -12,3 +12,5 @@ puts phrase
 puts phrase
 puts phrase
 end
+
+say_hello_world_ten_times
